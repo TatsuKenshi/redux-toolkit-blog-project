dc76cb4b67ff5039198f33ca6c77d585f36e0ac7
TatsuKenshi <rockinbg@gmail.com>
2022-09-21 17:02:37 +0200

    adds user pages with links to their posts

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import PostsList from "./features/posts/PostsList";
 import AddPostForm from "./features/posts/AddPostsForm";
 import SinglePostPage from "./features/posts/SinglePostPage";
 import EditPostForm from "./features/posts/EditPostForm";
+import UsersList from "./features/users/UsersList";
+import UserPage from "./features/users/UserPage";
 import Layout from "./components/Layout";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
     return (
@@ -15,6 +17,14 @@ function App() {
                     <Route index element={<AddPostForm />} />
                     <Route path=":postId" element={<SinglePostPage />} />
                     <Route path="edit/:postId" element={<EditPostForm />} />
+                </Route>
+
+                <Route path="user">
+                    <Route index element={<UsersList />} />
+                    <Route path=":userId" element={<UserPage />} />
+
+                    {/* Catch all - replaces the error page */}
+                    <Route path="*" element={<Navigate to="/" replace />} />
                 </Route>
             </Route>
         </Routes>
